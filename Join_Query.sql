3c1ca72eefb3f4fad7517808a6c7514c25223101
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-07-08 21:16:02 +0530

    Update Join_Query.sql

--- Join_Query.sql
+++ Join_Query.sql
@@ -1,3 +1,4 @@
+-- Wihout using JOIN --
 SET STATISTICS IO ON;
 SET STATISTICS TIME ON;
 
@@ -5,6 +6,8 @@ SELECT p.Name, Color, ListPrice
 FROM SalesLT.Product p, SalesLT.ProductCategory pc
 WHERE p.ProductCategoryID = pc.ProductCategoryID;
 
+
+-- Using JOIN --
 SET STATISTICS IO ON;
 SET STATISTICS TIME ON;
 
