67ff82ef09b32341161b69d7abd2448f79ec4dd6
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-07-08 21:15:16 +0530

    Update Indexing_query.sql

--- Indexing_query.sql
+++ Indexing_query.sql
@@ -1,9 +1,12 @@
+-- Creating Single Indexes --
 CREATE INDEX idx_CustomerID
 ON SalesLT.SalesOrderHeader (CustomerID);
 
+-- Creating Composite Indexes --
 CREATE INDEX idx_CustomerID_OrderDate
 ON SalesLT.SalesOrderHeader (CustomerID, OrderDate);
 
+-- Query on which indexes are applied --
 SELECT *
 FROM SalesLT.SalesOrderHeader
 WHERE CustomerID = 12345
