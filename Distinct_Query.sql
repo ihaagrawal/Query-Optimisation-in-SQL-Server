00d6cb59f4c9fdb7416231014b1daf73499b393b
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-07-08 21:13:07 +0530

    Update Distinct_Query.sql

--- Distinct_Query.sql
+++ Distinct_Query.sql
@@ -1,5 +1,8 @@
+-- Using SELECT DISTINCT --
 SET STATISTICS TIME ON
 SELECT DISTINCT Name, Color, StandardCost, Weight FROM SalesLT.Product
 
+
+--Without SELECT DISTINCT --
 SET STATISTICS TIME ON
 SELECT  Name, Color, StandardCost, Weight, SellEndDate, SellEndDate FROM SalesLT.Product
