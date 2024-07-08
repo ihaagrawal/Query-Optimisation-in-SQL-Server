67b7a20216e61166761317815e9d5eeb3aeac8ec
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-07-08 21:13:48 +0530

    Update and rename In_Query.sql to In_vs_Exists_Query.sql

--- In_vs_Exists_Query.sql
+++ In_vs_Exists_Query.sql
@@ -1,8 +1,11 @@
+-- Using IN --
 SET STATISTICS TIME ON
 SELECT ProductNumber,Name,Color FROM SalesLT.Product
 WHERE ProductID IN
 (SELECT ProductID FROM SalesLT.ProductDescription)
 
+
+-- Using EXISITS --
 SET STATISTICS TIME ON
 SELECT ProductNumber,Name,Color FROM SalesLT.Product
 WHERE  EXISTS
